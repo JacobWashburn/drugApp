121754662b08440538c9234b7ed72e0dc60d540b
Jacob Washburn <jacobrwashburn@gmail.com>
2024-02-26 15:36:54 -0700

    review page fix

--- svelte/src/routes/quizes/[id]/review/+page.svelte
+++ svelte/src/routes/quizes/[id]/review/+page.svelte
@@ -18,7 +18,7 @@
 	};
 </script>
 
-<div class="flex flex-col justify-center items-center p-12">
+<div class="flex flex-col w-full justify-center items-center p-12">
 	<div class="self-start">
 		<i
 			class="fas fa-arrow-alt-circle-left text-2xl"
